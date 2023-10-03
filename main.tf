e1503cdaaa21b3f7b498d6c36dd260739fa8630e
Hari Prasad M <hari.prasad@outsystems.com>
2023-10-03 16:26:43 +0530

    changed s3

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket-1"
+  bucket = "my-tf-test-bucket-2"
 
   tags = {
-    Name        = "My bucket"
+    Name        = "My bucket 1"
     Environment = "Dev"
   }
 }
