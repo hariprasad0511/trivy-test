1f60b0316a7b7364848e5520cecccc85785520d0
Hari Prasad M <hari.prasad@outsystems.com>
2023-10-17 18:35:01 +0530

    for test

--- s3.tf
+++ s3.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "example" {
   bucket = "my-tf-test-bucket-2"
 
   tags = {
-    Name        = "My bucket 2"
+    Name        = "My bucket 1"
     Environment = "Dev"
   }
 }
